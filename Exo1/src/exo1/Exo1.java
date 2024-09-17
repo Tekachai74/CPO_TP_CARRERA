09dcd3ec98031d361bfc7257d4a9147a1d7a1e8e
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-09-17 12:58:13 +0200

    Création d'un nouveau projet versionné

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,32 @@
+/*
+ * TP0 exercice1 
+ * VC
+ * Septembre 2024
+
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author VC
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+        System.out.println("Au revoir");
+        
+    }
+    
+}
