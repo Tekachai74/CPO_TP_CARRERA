f5bd32eda781b9439e1b2dc70bd00db4792340b7
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-09-20 18:15:02 +0200

    Création d'un nouveau projet versionné

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -1,20 +1,41 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * TP0 Exercice 2
+ * VC
+ * Septembre 2024
+ 
  */
 package exo2;
 
+import java.util.Scanner;
+
 /**
  *
- * @author valen
+ * @author VC
  */
 public class Exo2 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+     public static void main(String[] args) {
+ //Declaration des variables
+ int nb;
+ //nb=5;
+ Scanner sc = new Scanner(System.in);
+ System.out.println("\n Entrer le nombre :");
+ nb=sc.nextInt(); // On demande a sc de donner le prochain entier // nombre dentiers a additionner
+ int result; // resultat
+ int ind = 0; //indice
+ result=0;
+ // Addition des nb premiers entiers
+ ind=1;
+ while (ind <= nb) {
+ result=result+ind;
+ ind=ind+1;
+ }
+ // Affichage du resultat
+ System.out.println();
+ System.out.println("La somme des "+ nb + "entiers est: "+result);
     }
     
 }
