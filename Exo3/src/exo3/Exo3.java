df80c92ea9556c26534e15439ce4287ebe402ada
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-09-20 18:47:05 +0200

    Création d'un nouveau projet versionné

---
+++ Exo3/src/exo3/Exo3.java
@@ -0,0 +1,32 @@
+/*
+ * TP0 Exercice3
+ * VC
+ * Septembre 2024
+ */
+package exo3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author VC
+ */
+public class Exo3 {
+
+    /**
+     */
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter the operator:");
+        System.out.println("1) add");
+        System.out.println("2) subtract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
+        
+    int operateur = scanner.nextInt();
+    
+    scanner.close();
+    }
+}
