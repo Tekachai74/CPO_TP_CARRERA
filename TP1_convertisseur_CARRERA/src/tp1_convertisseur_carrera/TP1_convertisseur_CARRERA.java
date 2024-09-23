33f5c66524bf710d76d8563f3c17534e1940ff1f
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-09-23 12:27:57 +0200

    Création d'un nouveau projet versionné

--- TP1_convertisseur_CARRERA/src/tp1_convertisseur_carrera/TP1_convertisseur_CARRERA.java
+++ TP1_convertisseur_CARRERA/src/tp1_convertisseur_carrera/TP1_convertisseur_CARRERA.java
@@ -5,6 +5,8 @@
  */
 package tp1_convertisseur_carrera;
 
+import java.util.Scanner;
+
 /**
  *
  * @author VC
@@ -15,7 +17,14 @@ public class TP1_convertisseur_CARRERA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Entrez un reel: ");
+        double valeur = scanner.nextDouble();
+        double Kelvin = valeur + 273;
+        
+        System.out.println("La temperature en Celsius est de : " + valeur + "C");
+        System.out.println("La temperature en Kelvin est de : " + Kelvin + "K");
+        scanner.close();
     }
-    
 }
