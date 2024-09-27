92099376de4c14c4f3056382c2b95e5e01c96e97
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-09-27 10:37:27 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_stats_CARRERA/src/tp1_stats_carrera/TP1_stats_CARRERA.java
@@ -0,0 +1,21 @@
+/*
+ * TP1
+ * Valentin CARRERA
+ * Septembre 2024
+ */
+package tp1_stats_carrera;
+
+/**
+ *
+ * @author VC
+ */
+public class TP1_stats_CARRERA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
