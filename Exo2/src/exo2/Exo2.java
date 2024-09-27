ede5b0bd68759a08a936b974246abb02a6af1862
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-09-27 11:12:27 +0200

    Création d'un nouveau projet versionné

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -18,16 +18,16 @@ public class Exo2 {
      * @param args the command line arguments
      */
      public static void main(String[] args) {
- //Declaration des variables
+ 
  int nb;
  //nb=5;
  Scanner sc = new Scanner(System.in);
  System.out.println("\n Entrer le nombre :");
- nb=sc.nextInt(); // On demande a sc de donner le prochain entier // nombre dentiers a additionner
- int result; // resultat
- int ind = 0; //indice
+ nb=sc.nextInt(); 
+ int result;
+ int ind = 0;
  result=0;
- // Addition des nb premiers entiers
+
  ind=1;
  while (ind <= nb) {
  result=result+ind;
