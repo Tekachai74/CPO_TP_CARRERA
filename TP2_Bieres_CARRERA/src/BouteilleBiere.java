b2d12233fb39029a2260972ac327ccd969e2d4db
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-10-11 08:34:12 +0200

    Création d'un nouveau projet versionné

--- TP2_Bieres_CARRERA/src/BouteilleBiere.java
+++ TP2_Bieres_CARRERA/src/BouteilleBiere.java
@@ -12,4 +12,9 @@ public class BouteilleBiere {
     float degreAlcool;
     String brasserie;
     boolean ouverte;
+    
+public void lireEtiquette() { 
+    System.out.println("Bouteille de " + nom +" (" + degreAlcool + 
+    " degres) \nBrasserie : " + brasserie ) ;
+    }
 }
