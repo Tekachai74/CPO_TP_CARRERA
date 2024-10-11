8f9a7333393c30c89f08f587b77c7841f2b7810b
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-10-11 09:05:27 +0200

    Création d'un nouveau projet versionné

--- TP2_Bieres_CARRERA/src/tp2_bieres_carrera/TP2_Bieres_CARRERA.java
+++ TP2_Bieres_CARRERA/src/tp2_bieres_carrera/TP2_Bieres_CARRERA.java
@@ -14,13 +14,12 @@ public class TP2_Bieres_CARRERA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere() ;
-    uneBiere.nom = "Cuvée des trolls"; 
-    uneBiere.degreAlcool = 7.0 ; 
-    uneBiere.brasserie = "Dubuisson";
-    uneBiere.booleen = " non ouvert";
-    uneBiere.lireEtiquette(); 
-    
+    BouteilleBiere uneBiere = new BouteilleBiere();
+        uneBiere.nom = "Cuvée des trolls";
+        uneBiere.degreAlcool = 7.0; 
+        uneBiere.brasserie = "Dubuisson";
+        uneBiere.booleen = "non ouvert"; 
+
+        uneBiere.lireEtiquette();
     }
-    
 }
