92e309c65ac7d900508eef1cc42898e037396169
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-10-11 08:29:14 +0200

    Création d'un nouveau projet versionné

--- TP2_Bieres_CARRERA/src/BouteilleBiere.java
+++ TP2_Bieres_CARRERA/src/BouteilleBiere.java
@@ -1,12 +1,15 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * CARRERA Valentn
+ * Octobre 2024
  */
 
 /**
  *
- * @author valen
+ * @author VC
  */
 public class BouteilleBiere {
-    
+    String nom;
+    float degreAlcool;
+    String brasserie;
+    boolean ouverte;
 }
