791ac8d6a4c6dbb2a6a5cf05eb01fc00f4142241
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-10-16 10:27:08 +0200

    Création d'un nouveau projet versionné

--- TP2_Bieres_CARRERA/src/tp2_bieres_carrera/TP2_Bieres_CARRERA.java
+++ TP2_Bieres_CARRERA/src/tp2_bieres_carrera/TP2_Bieres_CARRERA.java
@@ -14,12 +14,15 @@ public class TP2_Bieres_CARRERA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere();
-        uneBiere.nom = "Cuvée des trolls";
-        uneBiere.degreAlcool = 7.0;
-        uneBiere.brasserie = "Dubuisson";
-        uneBiere.ouverte = false; 
-        uneBiere.lireEtiquette();
-    }
-}
+        BouteilleBiere uneBiere = new BouteilleBiere("CuvEe des trolls", 7.0 ,"Dubuisson") ;
+        BouteilleBiere autreBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ;
+        BouteilleBiere Biere = new BouteilleBiere("La chouffe", 5.0 ,"Abbaye de Chouffe") ;
+        BouteilleBiere powerade = new BouteilleBiere("Boisson",10.0 ,"ELLLLLL PRIMO") ;
 
+        powerade.lireEtiquette();
+        powerade.Decapsuler();
+       
+        System.out.println(powerade.ouverte);
+       
+
+}}
