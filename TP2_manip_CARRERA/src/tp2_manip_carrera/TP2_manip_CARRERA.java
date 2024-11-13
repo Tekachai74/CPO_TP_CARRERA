ae8a9c74286daced8f155a573d7f7baa90f797ee
Valentin CARRERA <valentin.carrera@epfedu.fr>
2024-11-13 15:59:23 +0100

    Création d'un nouveau projet versionné

--- TP2_manip_CARRERA/src/tp2_manip_carrera/TP2_manip_CARRERA.java
+++ TP2_manip_CARRERA/src/tp2_manip_carrera/TP2_manip_CARRERA.java
@@ -14,7 +14,20 @@ public class TP2_manip_CARRERA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+       
+        Moussaka [] tabMouss = new Moussaka[10];
+        tabMouss[0] = new Moussaka(0);
+        tabMouss[1] = new Moussaka(10);
+        tabMouss[2] = new Moussaka(20);
+        tabMouss[3] = new Moussaka(30);
+        tabMouss[4] = new Moussaka(40);
+        tabMouss[5] = new Moussaka(50);
+        tabMouss[6] = new Moussaka(60);
+        tabMouss[7] = new Moussaka(70);
+        tabMouss[9] = new Moussaka(80);
+        tabMouss[8] = new Moussaka(90);
     }
-    
+   
 }
